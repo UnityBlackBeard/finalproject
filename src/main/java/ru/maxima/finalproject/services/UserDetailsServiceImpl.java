efdcbec0931e8005a0456148626b0691024d4b4b
UnityBlackBeard <vladvasilevs@gmail.com>
2023-04-09 20:27:08 +0300

    Initial commit

---
+++ src/main/java/ru/maxima/finalproject/services/UserDetailsServiceImpl.java
@@ -0,0 +1,42 @@
+package ru.maxima.finalproject.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+//import ru.maxima.finalproject.models.User;
+import ru.maxima.finalproject.models.Person;
+import ru.maxima.finalproject.repositories.PersonRepository;
+
+import java.util.stream.Collectors;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+    @Autowired
+    public PersonRepository personRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+
+        Person currUser = personRepository.findByUsername(username).orElse(null);
+        if (currUser == null) {
+            throw new UsernameNotFoundException("Unknown user: "+username);
+        }
+        UserDetails user = User.builder()
+                .username(currUser.getUsername())
+                .password(currUser.getPassword())
+                .authorities(
+                        currUser.getRoles()
+                                .stream()
+                                .map(role -> new SimpleGrantedAuthority(role.getName().name()))
+                                .collect(Collectors.toList())
+                )
+                .build();
+        return user;
+
+    }
+}
